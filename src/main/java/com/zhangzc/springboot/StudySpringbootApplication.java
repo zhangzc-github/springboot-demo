f7f498ca278d227a0ba3444d728b2aa82ca804b9
zhangzc-github <1843868984@qq.com>
2020-03-11 22:01:52 +0800

    Initial commit

---
+++ src/main/java/com/zhangzc/springboot/StudySpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.zhangzc.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudySpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudySpringbootApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/zhangzc/springboot/controller/LoginController.java
@@ -0,0 +1,12 @@
+package com.zhangzc.springboot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class LoginController {
+    @RequestMapping("/index")
+    public String index(){
+        return "index";
+    }
+}
