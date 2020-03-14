3d0948c4a8cd96e219d4b3c1058fd2ba3769275e
zhangzc-github <1843868984@qq.com>
2020-03-14 13:53:26 +0800

    员工列表完成

--- src/main/java/com/zhangzc/springboot/config/MyWebConfig.java
+++ src/main/java/com/zhangzc/springboot/config/MyWebConfig.java
@@ -1,6 +1,11 @@
 package com.zhangzc.springboot.config;
 
+import com.zhangzc.springboot.config.component.LoginInterceptor;
+import com.zhangzc.springboot.config.component.MyLocaleResolver;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -9,5 +14,18 @@ public class MyWebConfig implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/").setViewName("index");
+        registry.addViewController("/main").setViewName("dashboard");
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoginInterceptor())
+                .addPathPatterns("/**")
+                .excludePathPatterns("/","/user/login","/webjars/**","/favicon.ico","/asserts/**");
+    }
+
+    @Bean
+    public LocaleResolver localeResolver(){
+        return new MyLocaleResolver();
     }
 }
--- src/main/java/com/zhangzc/springboot/controller/LoginController.java
+++ src/main/java/com/zhangzc/springboot/controller/LoginController.java
@@ -1,12 +1,42 @@
 package com.zhangzc.springboot.controller;
 
+import com.zhangzc.springboot.dao.EmployeeDao;
+import com.zhangzc.springboot.entities.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpSession;
+import java.util.Collection;
+import java.util.Map;
 
 @Controller
 public class LoginController {
-    @RequestMapping("/index")
-    public String index(){
+
+    @Autowired
+    private EmployeeDao employeeDao;
+
+    @RequestMapping("/user/login")
+    public String login(@RequestParam("username") String username,
+                        @RequestParam("password") String password,
+                        Map<String, Object> map,
+                        HttpSession session) {
+        if (!StringUtils.isEmpty(username) && "123".equals(password)) {
+            session.setAttribute("user", username);
+            return "redirect:/main";
+        }
+        map.put("msg", "登录失败!");
         return "index";
+    }
+
+    @GetMapping("/emps")
+    public String getEmps(Model model) {
+        Collection<Employee> emps = employeeDao.getAll();
+        model.addAttribute("emps",emps);
+        return "list";
     }
 }
